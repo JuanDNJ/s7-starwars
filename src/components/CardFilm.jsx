59a3db2a83ff53816aaf2f4371f44ae39597a818
JuanDNJ <juandevnjv@gmail.com>
2024-04-27 00:58:35 +0200

    "modified:   src/assets/css/style.css"

--- src/components/CardFilm.jsx
+++ src/components/CardFilm.jsx
@@ -17,8 +17,10 @@ export default function CardFilm({ film }) {
   return (
     <div className="bg-[rgba(0,0,0,.5)] relative rounded-t-xl overflow-hidden border border-stone-700">
       <div className="absolute bottom-0 bg-[rgba(0,0,0,.9)] w-full border-t-2 border-yellow-600 py-1 px-4">
-        <h2 className="text-lg text-stone-300">{cloneFilm.title}</h2>
-        <div className="flex gap-4 text-blue-400 text-sm">
+        <h2 className="text-[.9rem] md:text-[1rem] text-stone-300">
+          {cloneFilm.title}
+        </h2>
+        <div className="flex gap-4 text-blue-400 text-[.6rem] md:text-[.8rem]">
           <strong>Director:</strong>
           <span>{cloneFilm.director}</span>
         </div>
--- src/components/CardPilots.jsx
+++ src/components/CardPilots.jsx
@@ -18,7 +18,7 @@ export default function CardPilot({ pilot }) {
   return (
     <article className="bg-[rgba(0,0,0,.5)] relative  rounded-t-xl overflow-hidden border border-stone-700">
       <div className="absolute bottom-0 bg-[rgba(0,0,0,.9)] w-full px-4 py-2 border-t-2 border-yellow-600 ">
-        <h2 className="text-lg text-stone-300">{clonePilot.name}</h2>
+        <h2 className="text-sm text-stone-300">{clonePilot.name}</h2>
       </div>
       <img src={clonePilot.newUrl} alt="Movie" />
     </article>
