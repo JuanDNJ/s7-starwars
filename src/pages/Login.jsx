e9f23c33228aa2770a8182b5ce832aee9b40426c
JuanDNJ <juandevnjv@gmail.com>
2024-04-27 10:20:56 +0200

    "modified:   Signup.jsx, Login.jsx"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,62 +40,66 @@ export default function Login() {
     dispatch(setErrorCode(null));
   }, [isLogin]);
   return (
-    <section className="flex flex-col gap-4 items-center justify-center min-h-[50vh]">
-      <h2 className="font-title text-yellow-400">
-        Enter your user information
-      </h2>
+    !isLogin && (
+      <section className="flex flex-col gap-4 items-center justify-center min-h-[50vh]">
+        <article className="flex flex-col items-center">
+          <h2 className="font-title text-yellow-400 ">
+            Enter your user information
+          </h2>
 
-      <form
-        onSubmit={handlerLogin}
-        className="flex flex-col items-center justify-center w-[320px] md:max-w-[320px] h-[25vh]  gap-4  py-4 px-8 relative"
-      >
-        <ErrorFirebase />
-        <input
-          className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white w-full"
-          name="email"
-          type="email"
-          placeholder="E-mail"
-          required
-        />
-        <div className="w-full form-control">
-          <label
-            htmlFor="password"
-            className="password pr-4 active:scale-50 duration-500"
-            onClick={handlerViewPassword}
+          <form
+            onSubmit={handlerLogin}
+            className="flex flex-col items-center justify-center w-[320px] md:max-w-[320px] h-[25vh] gap-4 py-4 px-8 relative"
           >
-            <img
-              src={viewPasswordIcon}
-              width={28}
-              height={28}
-              alt="View password"
+            <ErrorFirebase />
+            <input
+              className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white w-full"
+              name="email"
+              type="email"
+              placeholder="E-mail"
+              required
             />
-          </label>
-          <input
-            className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white  w-full"
-            name="password"
-            id="password"
-            type={`${(viewPassword && "text") || "password"}`}
-            placeholder="Password"
-            required
-          />
-        </div>
+            <div className="w-full form-control">
+              <label
+                htmlFor="password"
+                className="password pr-4 active:scale-50 duration-500"
+                onClick={handlerViewPassword}
+              >
+                <img
+                  src={viewPasswordIcon}
+                  width={28}
+                  height={28}
+                  alt="View password"
+                />
+              </label>
+              <input
+                className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white  w-full"
+                name="password"
+                id="password"
+                type={`${(viewPassword && "text") || "password"}`}
+                placeholder="Password"
+                required
+              />
+            </div>
 
-        <div className="flex justify-between w-full gap-4">
-          <button
-            type="submit"
-            className="bg-green-200 rounded-md p-2 text-stone-800 flex-1"
-          >
-            Login
-          </button>
-          <button
-            type="reset"
-            className="bg-red-200 rounded-md p-2 text-stone-800  flex-1"
-          >
-            Reset
-          </button>
-        </div>
-      </form>
-      <GoToPage url="/signup"> You do not have an account?</GoToPage>
-    </section>
+            <div className="flex justify-between w-full gap-4">
+              <button
+                type="submit"
+                className="bg-green-200 rounded-md p-2 text-stone-800 flex-1"
+              >
+                Login
+              </button>
+              <button
+                type="reset"
+                className="bg-red-200 rounded-md p-2 text-stone-800  flex-1"
+              >
+                Reset
+              </button>
+            </div>
+          </form>
+          <GoToPage url="/signup"> You do not have an account?</GoToPage>
+        </article>
+      </section>
+    )
   );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -40,56 +40,58 @@ export default function Signup() {
   return (
     !isLogin && (
       <section className="flex flex-col gap-4 items-center justify-center min-h-[50vh]">
-        <h2 className="font-title text-yellow-400">Create new account</h2>
-        <form
-          onSubmit={handlerSubmit}
-          className="flex flex-col items-center justify-center w-[320px] md:max-w-[320px]  gap-4  py-4 px-8 relative"
-        >
-          <ErrorFirebase />
-          <input
-            className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white  w-full"
-            name="email"
-            type="email"
-            required
-            placeholder="E-mail"
-          />
-          <div className="w-full form-control">
-            <label
-              htmlFor="password"
-              className="password pr-4 active:scale-50 duration-500"
-              onClick={handlerViewPassword}
-            >
-              <img
-                src={viewPasswordIcon}
-                width={28}
-                height={28}
-                alt="View password"
-              />
-            </label>
+        <article className="text-center">
+          <h2 className="font-title text-yellow-400 ">Create new account</h2>
+          <form
+            onSubmit={handlerSubmit}
+            className="flex flex-col items-center justify-center w-[320px] md:max-w-[320px] h-[25vh] gap-4 py-4 px-8 relative"
+          >
+            <ErrorFirebase />
             <input
               className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white  w-full"
-              name="password"
-              type={`${(viewPassword && "text") || "password"}`}
-              placeholder="Password"
+              name="email"
+              type="email"
               required
+              placeholder="E-mail"
             />
-          </div>
-          <div className="flex justify-between w-full gap-4">
-            <button
-              className="bg-green-200 rounded-md p-2  text-stone-800 flex-1"
-              type="submit"
-            >
-              Sign Up
-            </button>
-            <button
-              className="bg-red-200 rounded-md p-2  text-stone-800 flex-1"
-              type="reset"
-            >
-              Reset
-            </button>
-          </div>
-        </form>
-        <GoToPage url="/login">You have an account?</GoToPage>
+            <div className="w-full form-control">
+              <label
+                htmlFor="password"
+                className="password pr-4 active:scale-50 duration-500"
+                onClick={handlerViewPassword}
+              >
+                <img
+                  src={viewPasswordIcon}
+                  width={28}
+                  height={28}
+                  alt="View password"
+                />
+              </label>
+              <input
+                className="p-2 rounded-md ring-1 ring-yellow-400 bg-black text-white  w-full"
+                name="password"
+                type={`${(viewPassword && "text") || "password"}`}
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="flex justify-between w-full gap-4">
+              <button
+                className="bg-green-200 rounded-md p-2  text-stone-800 flex-1"
+                type="submit"
+              >
+                Sign Up
+              </button>
+              <button
+                className="bg-red-200 rounded-md p-2  text-stone-800 flex-1"
+                type="reset"
+              >
+                Reset
+              </button>
+            </div>
+          </form>
+          <GoToPage url="/login">You have an account?</GoToPage>
+        </article>
       </section>
     )
   );
